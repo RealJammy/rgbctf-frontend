9b7f2cf0c65c3c633441fc0c2c212d100bb98dcd
Alec Petridis <alec@rgbsec.xyz>
2020-05-25 23:30:54 -0700

    clippyclippyclippyclippyclippyclippyclippy

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-
+import clippy from 'clippyjs'
 import Navbar from './components/Navbar'
 import { ThemeProvider } from '@react95/core';
 
 
 
 function App() {
+  clippy.load('Clippy', function(agent){
+    // do anything with the loaded agent
+    agent.show();
+});
   return (
+    
     <ThemeProvider>   <Navbar/>
     </ThemeProvider>
 
