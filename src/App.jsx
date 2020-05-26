18019bdf657582d8a513918dee69a9dfc4142086
Alec Petridis <alec@rgbsec.xyz>
2020-05-26 15:01:22 -0700

    challenges are now kinda dynamic, cant work out why the fuck 43232423 clippys spawn when i make a new window

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React from "react";
 
-import clippy from 'clippyjs'
-import Navbar from './components/Navbar'
-import { ThemeProvider } from '@react95/core';
+import clippy from "clippyjs";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "@react95/core";
+import ChallengeModal from "./components/ChallengeModal";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      challenges: [
+        {
+          name: "abc",
+          category: "OSINT",
+        },
+      ],
+      challengeModals: []
+    };
+    this.handleOpen = this.handleOpen.bind(this)
+  }
 
+  handleOpen(challenge) {
+    this.setState({
+      challenges: this.state.challenges,
+      challengeModals: this.state.challengeModals.concat(<ChallengeModal name={challenge.name} category={challenge.category}></ChallengeModal>)
+    })
+  }
 
-function App() {
-  clippy.load('Clippy', function(agent){
-    // do anything with the loaded agent
-    agent.show();
-});
-clippy.load('Bonzi', function(agent){
-  // do anything with the loaded agent
-  agent.show();
-});
-  return (
-    
-    <ThemeProvider>   <Navbar/>
-    </ThemeProvider>
+  render() {
+    clippy.load("Clippy", function (agent) {
+      // do anything with the loaded agent
+      agent.show();
+    });
+    clippy.load("Bonzi", function (agent) {
+      // do anything with the loaded agent
+      agent.show();
+    });
 
-  );
+    return (
+      <ThemeProvider>
+        {this.state.challengeModals.map((chall) => {
+          console.log(chall);
+          return chall;
+        })}
+
+        <Navbar challenges={this.state.challenges} handleOpen={this.handleOpen}/>
+      </ThemeProvider>
+    );
+  }
 }
-
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,70 +2,82 @@ import React, { useState } from "react";
 import TaskBar from "@react95/core/TaskBar";
 import { Modal } from "@react95/core/Modal";
 import List from "@react95/core/List";
+import ChallengeModal from "./ChallengeModal";
 
-const NavBar = () => {
-  const [logout, doLogout] = useState(false);
+import ReactDom from "react-dom";
 
-  return (
-    <>
-      {logout && (
-        <Modal
-          defaultPosition={{ x: 50, y: 50 }}
-          width={300}
-          height={200}
-          icon="reader_closed"
-          title="blah blah blah logout here"
+import context from "../lib/context";
+
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logout: false,
+      doLogout: () => (this.state.logout = true),
+    };
+  }
+
+
+
+  render() {
+    return (
+      <>
+        <TaskBar
+          list={
+            <>
+              <List>
+                <List.Item icon="shell32_135">
+                  <List>
+                    {this.props.challenges
+                      .filter((chall) => chall.category === "OSINT")
+                      .map((chall) => (
+                        <List.Item key={chall.name} onClick={() => this.props.handleOpen(chall)}>
+                          abc
+                        </List.Item>
+                      ))}
+
+                    <List.Item icon="shell32_135">test2</List.Item>
+                  </List>
+                  OSINT
+                </List.Item>
+                <List.Item icon="shdocvw_259">
+                  <List>
+                    <List.Item icon="shell32_135">test3</List.Item>
+                    <List.Item icon="shell32_135">test4</List.Item>
+                  </List>
+                  Crypto
+                </List.Item>
+                <List.Item icon="shdocvw_257">
+                  <List>
+                    <List.Item>test5</List.Item>
+                    <List.Item>test6</List.Item>
+                  </List>
+                  Web
+                </List.Item>
+                <List.Divider />
+
+                <List.Item icon="folder_file">Documents</List.Item>
+                <List.Item icon="settings">
+                  <List>
+                    <List.Item icon="folder_settings">
+                      Account Profile
+                    </List.Item>
+                    <List.Item icon="folder_print">Team Settings</List.Item>
+                  </List>
+                  Settings
+                </List.Item>
+                <List.Divider />
+                <List.Item icon="computer_3">Shut Down...</List.Item>
+              </List>
+
+              <br />
+            </>
+          }
         />
-      )}
-
-      <TaskBar
-        list={
-          <>
-            <List>
-              
-            
-                  <List.Item icon="shell32_135">
-                    <List>
-                      <List.Item icon="shell32_135">test1</List.Item>
-                      <List.Item icon="shell32_135">test2</List.Item>
-                    </List>
-                    OSINT
-                  </List.Item>
-                  <List.Item icon="shdocvw_259">
-                    <List>
-                      <List.Item icon="shell32_135">test3</List.Item>
-                      <List.Item icon="shell32_135">test4</List.Item>
-                    </List>
-                    Crypto
-                  </List.Item>
-                  <List.Item icon="shdocvw_257">
-                    <List>
-                      <List.Item>test5</List.Item>
-                      <List.Item>test6</List.Item>
-                    </List>
-                    Web
-                  </List.Item>
-               <List.Divider/>
-
-
-              <List.Item icon="folder_file">Documents</List.Item>
-              <List.Item icon="settings">
-                <List>
-                  <List.Item icon="folder_settings">Account Profile</List.Item>
-                  <List.Item icon="folder_print">Team Settings</List.Item>
-                </List>
-                Settings
-              </List.Item>
-              <List.Divider />
-              <List.Item icon="computer_3">Shut Down...</List.Item>
-            </List>
-
-            <br />
-          </>
-        }
-      />
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default NavBar;
